8dea1dd248867e1e17b7282cae96d85b0e95bbc8
Amol suradkar <36931368+amolsuradkar@users.noreply.github.com>
2024-06-11 10:19:40 +0000

    workflow change 8

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -52,6 +52,7 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+    print("Data ingested successfully")
 
     # data_transformation=DataTransformation()
     # train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
